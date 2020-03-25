bdde66556201544892020371f35947942d6e667f
Jorge Teixeira <jorgeteixe11@gmail.com>
2020-03-25 20:09:32 +0100

    changed teacher to teacher.name

--- models/mclass.py
+++ models/mclass.py
@@ -18,4 +18,4 @@ class Class(models.Model):
     @api.depends('title', 'teacher_id')
     def _full_name(self):
         for r in self:
-            r.name = r.title + ' ' + r.teacher_id
+            r.name = r.title + ' ' + r.teacher_id.name
